f12e2eb161889fb4baec09b9e40ae47c789d41ed
Abel Nigus <abinig5@gmail.com>
2024-04-22 12:48:54 +0300

    add connection string

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -5,7 +5,7 @@ import { local_config } from "../config/config";
 (async () => {
   try {
     set("strictQuery", false);
-    const db = await connect(local_config.MONGO_DB_CONNECTION);
+    const db = await connect(local_config.MONGO_DB_URI);
     console.log("MongoDB connected to", db.connection.name);
   } catch (error) {
     console.error(error);
