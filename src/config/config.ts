22018313fe0bacf60031bf0e232303c14d73220e
Abel Nigus <abinig5@gmail.com>
2024-04-22 12:18:49 +0300

    add connection string

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,5 +2,5 @@ import { config } from "dotenv";
 config();
 
 export const MONGO_DB_CONNECTION =
-  process.env.MONGO_DB_URI
+  process.env.MONGO_DB_URI || "mongodb+srv://ruthpp3:IalFOQzAlNjkLFI1@cluster0.u8gon1l.mongodb.net/merin?retryWrites=true&w=majority";
 
