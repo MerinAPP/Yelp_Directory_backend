b87773857cfb2aebfee47eec0b25a435db70c043
Abel Nigus <abinig5@gmail.com>
2024-05-16 10:36:40 +0300

    remove_pkg

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,30 +1,30 @@
-import { v2 as cloudinary } from "cloudinary";
-import { config } from "dotenv";
-import { local_config } from "./config";
-config();
+// import { v2 as cloudinary } from "cloudinary";
+// import { config } from "dotenv";
+// import { local_config } from "./config";
+// config();
 
-cloudinary.config({
-  cloud_name: local_config.CLOUD_NAME,
-  api_key: local_config.CLOUDINARY_API_KEY,
-  api_secret: local_config.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: local_config.CLOUD_NAME,
+//   api_key: local_config.CLOUDINARY_API_KEY,
+//   api_secret: local_config.CLOUDINARY_API_SECRET,
+// });
 
-export const uploads = (file, folder) => {
-  return new Promise((resolve, reject) => {
-    console.log({ file });
-    cloudinary.uploader
-      .upload(file, {
-        resource_type: "auto",
-        folder: folder,
-      })
-      .then((result) => {
-        resolve({
-          url: result.url,
-          id: result.public_id,
-        });
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
-};
+// export const uploads = (file, folder) => {
+//   return new Promise((resolve, reject) => {
+//     console.log({ file });
+//     cloudinary.uploader
+//       .upload(file, {
+//         resource_type: "auto",
+//         folder: folder,
+//       })
+//       .then((result) => {
+//         resolve({
+//           url: result.url,
+//           id: result.public_id,
+//         });
+//       })
+//       .catch((error) => {
+//         reject(error);
+//       });
+//   });
+// };
